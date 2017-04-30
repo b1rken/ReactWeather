90aef24e6feb1e035a160d9d26c0c68d47a4e29b
Espen Birk <espenbirk@Espens-MacBook-Pro.local>
2017-04-30 15:33:47 +0200

    Add Foundation & Jquery

--- app/app.jsx
+++ app/app.jsx
@@ -6,12 +6,16 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// Load Foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <Route path="about" component={About}/>
       <Route path="examples" component={Examples}/>
-      <IndexRoute component={Weather}/>
+        <IndexRoute component={Weather}/>
     </Route>
   </Router>,
   document.getElementById('app')
